ace844e1ac263aeaf919fcc9600cfb048f8b26bf
vikashkumar6207 <vktd828403@gmail.com>
2026-01-03 21:19:52 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,58 @@
+// require('dotenv').config()
+
+import dotenv from "dotenv"
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+import express from "express"
+const app = express();
+
+( async () => {
+    try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      app.on("errror", (error) => {
+        console.log("ERRr: ", error);
+        throw error;
+        
+      })
+      app.listen(process.env.PORT, ( ) => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+        
+      })
+
+    } catch (error){
+        console.log("ERROR: ", error);
+        throw error;
+        
+    }
+} )()
+
+*/
